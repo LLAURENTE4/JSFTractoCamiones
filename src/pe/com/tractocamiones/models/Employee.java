b7ac0d7004734741f485c221cec0eddd5cb42021
luis laurente <llaurente4@gmail.com>
2016-07-22 18:39:05 -0500

    Modificacion de tablas

--- src/pe/com/tractocamiones/models/Employee.java
+++ src/pe/com/tractocamiones/models/Employee.java
@@ -4,4 +4,105 @@ package pe.com.tractocamiones.models;
  * Created by llaurente on 22/07/2016.
  */
 public class Employee {
+
+private Integer idEmployee;
+private String idUser;
+private String fatherLastName;
+private String motherLastName;
+private String fullName;
+private String document;
+private String address;
+private String telephone;
+private String fax;
+private String emergencyNumber;
+private String status;
+
+
+    public Integer getIdEmployee() {
+        return idEmployee;
+    }
+
+    public void setIdEmployee(Integer idEmployee) {
+        this.idEmployee = idEmployee;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getFatherLastName() {
+        return fatherLastName;
+    }
+
+    public void setFatherLastName(String fatherLastName) {
+        this.fatherLastName = fatherLastName;
+    }
+
+    public String getMotherLastName() {
+        return motherLastName;
+    }
+
+    public void setMotherLastName(String motherLastName) {
+        this.motherLastName = motherLastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmergencyNumber() {
+        return emergencyNumber;
+    }
+
+    public void setEmergencyNumber(String emergencyNumber) {
+        this.emergencyNumber = emergencyNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/pe/com/tractocamiones/models/Machine.java
+++ src/pe/com/tractocamiones/models/Machine.java
@@ -4,4 +4,8 @@ package pe.com.tractocamiones.models;
  * Created by llaurente on 22/07/2016.
  */
 public class Machine {
+    String id;
+    String idMarc;
+    String nameMarc;
+
 }
